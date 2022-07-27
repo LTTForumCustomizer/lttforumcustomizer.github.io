63fabea757164f5d2d9abc96de105bdd39d691f6
LTT Forum Customizer <lttforumcustomizer@protonmail.com>
2022-07-27 15:00:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-let message = "You look at the console huh?";
+const message = "Good! You look at the console. You're not too bad after all.";
+const webDesigner = "EDKTech";
+const extensionDesigner = "RockSolid1106";
 
 console.log(message);
+console.log(`${webDesigner} made the website, ${extensionDesigner} made the extension. Duh!`);
+console.log('Jk, Jk');
